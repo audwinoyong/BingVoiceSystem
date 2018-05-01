79bee875aaa760412a77aea1c4678b62d32f4d0e
Sean <sean_mysell@hotmail.com>
2018-05-01 14:29:03 +1000

    Add reports page

---
+++ BingVoiceSystem/BingVoiceSystem/Reports.aspx
@@ -0,0 +1,16 @@
+﻿<%@ Page Title="Reports" Language="C#" MasterPageFile="~/Site.Master" AutoEventWireup="true" CodeBehind="Reports.aspx.cs" Inherits="BingVoiceSystem.Reports" %>
+
+<asp:Content ID="BodyContent" ContentPlaceHolderID="MainContent" runat="server">
+    <h2>Generate Reports:</h2>
+    <asp:Button ID="rulesReportBtn" runat="server" Text="Rules Report" OnClick="RulesReportBtn_Click" Height="38px" Width="155px" Visible="False" />
+    </br>
+    </br>
+    <asp:Button ID="editorReportBtn" runat="server" Text="Editor Report" OnClick="EditorReportBtn_Click" Height="38px" Width="155px" Visible="False" />
+    </br>
+    </br>
+    <asp:Button ID="approverReportBtn" runat="server" Text="Rules Report" OnClick="ApproverReportBtn_Click" Height="38px" Width="155px" Visible="False" />
+    </br>
+    </br>
+    <asp:Label ID="permissionsLbl" runat="server" Text="No reports to generate" Visible="False" />
+</asp:Content>
+
---
+++ BingVoiceSystem/BingVoiceSystem/Reports.aspx.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace BingVoiceSystem
+{
+    public partial class Reports : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!User.IsInRole("Editor") && !User.IsInRole("Approver"))
+            {
+                permissionsLbl.Visible = true;
+            }
+            else if (User.IsInRole("Editor"))
+            {
+                editorReportBtn.Visible = true;
+            }
+            else if (User.IsInRole("Approver"))
+            {
+                rulesReportBtn.Visible = true;
+                approverReportBtn.Visible = true;
+            }
+        }
+
+        protected void RulesReportBtn_Click(object sender, EventArgs e)
+        {
+        }
+
+        protected void EditorReportBtn_Click(object sender, EventArgs e)
+        {
+        }
+
+        protected void ApproverReportBtn_Click(object sender, EventArgs e)
+        {
+        }
+    }
+}
