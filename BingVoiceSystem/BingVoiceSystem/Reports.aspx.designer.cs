64e87aa3a050ce3dd2d16878f28ef372a3a76ef0
Audwin Oyong <11814771@student.uts.edu.au>
2018-05-04 19:05:09 +1000

    Enable screen permissions ; Fix typo in Reports screen

--- BingVoiceSystem/BingVoiceSystem/Reports.aspx.designer.cs
+++ BingVoiceSystem/BingVoiceSystem/Reports.aspx.designer.cs
@@ -13,15 +13,6 @@ namespace BingVoiceSystem {
     public partial class Reports {
         
         /// <summary>
-        /// rulesReportBtn control.
-        /// </summary>
-        /// <remarks>
-        /// Auto-generated field.
-        /// To modify move field declaration from designer file to code-behind file.
-        /// </remarks>
-        protected global::System.Web.UI.WebControls.Button rulesReportBtn;
-        
-        /// <summary>
         /// editorReportBtn control.
         /// </summary>
         /// <remarks>
@@ -29,6 +20,15 @@ namespace BingVoiceSystem {
         /// To modify move field declaration from designer file to code-behind file.
         /// </remarks>
         protected global::System.Web.UI.WebControls.Button editorReportBtn;
+        
+        /// <summary>
+        /// rulesReportBtn control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Button rulesReportBtn;
         
         /// <summary>
         /// approverReportBtn control.
