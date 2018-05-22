98a8c456d5006bd5f7ee245b253a1facdefb5a31
BuildTools <unconfigured@null.spigotmc.org>
2018-05-22 18:57:52 +1000

    Started on MVC for reports

---
+++ BingVoiceSystem/BingVoiceSystem.WebMVC/Controllers/ReportsController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using BingVoiceSystem.WebMVC.Models;
+
+namespace BingVoiceSystem.WebMVC.Controllers
+{
+    public class ReportsController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult ApproverReport()
+        {
+            return View();
+        }
+
+        public ActionResult EditorReport()
+        {
+            return View();
+        }
+
+        public ActionResult RulesReport()
+        {
+            return View();
+        }
+    }
+}
---
+++ BingVoiceSystem/BingVoiceSystem.WebMVC/Models/ReportsModels.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Web;
+
+namespace BingVoiceSystem.WebMVC.Models
+{
+    public class ReportsModels
+    {
+        public List<Rules> RulesList { get; set; } 
+
+    }
+}
