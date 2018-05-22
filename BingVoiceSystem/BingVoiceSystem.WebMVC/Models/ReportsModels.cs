27ee62d5785c5c3a88dd54d9a788157e8ba4a2c7
BuildTools <unconfigured@null.spigotmc.org>
2018-05-23 00:26:19 +1000

    Minor progress on Rules Report

--- BingVoiceSystem/BingVoiceSystem.WebMVC/Models/ReportsModels.cs
+++ BingVoiceSystem/BingVoiceSystem.WebMVC/Models/ReportsModels.cs
@@ -6,7 +6,10 @@ namespace BingVoiceSystem.WebMVC.Models
 {
     public class ReportsModels
     {
-        public List<Rules> RulesList { get; set; } 
+        public List<string> ApprovedRules { get; set; }
 
+        public string Question { get; set; }
+
+        public string Answer { get; set; }
     }
 }
