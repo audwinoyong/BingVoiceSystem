9e68196855c0fbae507af7faa9f3067b9c469ca4
Sean <sean_mysell@hotmail.com>
2018-05-25 01:11:33 +1000

    Add rules home page and related MVC components

---
+++ BingVoiceSystem/BingVoiceSystem.WebMVC/Models/RulesModel.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using BingVoiceSystem.Data;
+
+namespace BingVoiceSystem.WebMVC.Models
+{
+    public class RulesModel
+    {        
+        /// <summary>
+        /// The name of a new personal contact
+        /// </summary>
+        public string Name { get; set; }
+
+        /// <summary>
+        /// The address for a new personal contact
+        /// </summary>
+        public string Address { get; set; }
+    }
+}
