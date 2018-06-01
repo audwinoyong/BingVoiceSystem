471589ff9fa671c4e71919356e97b1c3335c5f0c
Daniel Turrisi <98124187@student.uts.edu.au>
2018-06-01 17:31:44 +1000

    Cleaned up report formats. Prettyfied buttons in rules list and added edit/delete buttons

--- BingVoiceSystem/BingVoiceSystem.WebMVC/Controllers/HomeController.cs
+++ BingVoiceSystem/BingVoiceSystem.WebMVC/Controllers/HomeController.cs
@@ -24,21 +24,5 @@ namespace BingVoiceSystem.WebMVC.Controllers
             model.SetAnswer(model.Question);
             return View(model);
         }
-        
-        
-
-        public ActionResult About()
-        {
-            ViewBag.Message = "Your application description page.";
-
-            return View();
-        }
-
-        public ActionResult Contact()
-        {
-            ViewBag.Message = "Your contact page.";
-
-            return View();
-        }
     }
 }
